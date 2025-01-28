08b881d54a57230cd4b6beecd5e3b12f9e54fcd8
kumaralok001 <105136972+kumaralok001@users.noreply.github.com>
2025-01-28 15:15:20 +0530

    Locators
    ID: Locates an element by its unique id attribute.
    XPath: A flexible locator that identifies elements using XML path syntax.
    CSS Selector: Selects elements using CSS rules for precise identification.
    Class Name: Locates elements by their class attribute.
    Name: Identifies elements by the name attribute.
    Link Text: Targets links by their visible text.

---
+++ SeleniumPythonTesting/pythonSelenium/locators.py
@@ -0,0 +1,39 @@
+import time
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+
+# ID, Xpath, CSSSelector, Classname, name, linkText
+driver.find_element(By.NAME, "email").send_keys("hello@gmail.com")
+driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
+driver.find_element(By.ID, "exampleCheck1").click()
+
+# Xpath -> //tagname[@attribute='value'] -> //input[@type='submit']
+# CSS -> tagname[attribute='value'] -> input[type='submit'], #id, .classname
+driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Rahul")
+driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
+
+
+
+driver.find_element(By.XPATH,"//input[@type='submit']").click()
+message = driver.find_element(By.CLASS_NAME, "alert-success").text
+print(message)
+
+assert "Success" in message
+
+driver.find_element(By.XPATH, "(//input[@type='text'])[3]").send_keys("RahulHelloAgain")
+driver.find_element(By.XPATH,"(//input[@type='text'])[3]").clear()
+
+
+
+
+
+
+
+
+
+time.sleep(10)

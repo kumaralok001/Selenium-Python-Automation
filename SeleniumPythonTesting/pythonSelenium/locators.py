0cdb389cf50ae054d9138ef8139c9eb5fe31135e
kumaralok001 <105136972+kumaralok001@users.noreply.github.com>
2025-01-29 19:08:52 +0530

    Updated pythonSelenium folder
    Added more .py files -- uiControls, alrets and findElementTest. Additionally, updated the locators file

--- SeleniumPythonTesting/pythonSelenium/locators.py
+++ SeleniumPythonTesting/pythonSelenium/locators.py
@@ -3,6 +3,7 @@ from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 driver = webdriver.Chrome()
 driver.get("https://rahulshettyacademy.com/angularpractice/")
@@ -17,8 +18,11 @@ driver.find_element(By.ID, "exampleCheck1").click()
 driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Rahul")
 driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
 
-
-
+#--------Static Dropdown----------------
+dropdown = Select(driver.find_element(By.ID, "exampleFormControlSelect1"))
+dropdown.select_by_visible_text("Female")
+dropdown.select_by_index(0)
+# dropdown.select_by_value() #if select class have values
 driver.find_element(By.XPATH,"//input[@type='submit']").click()
 message = driver.find_element(By.CLASS_NAME, "alert-success").text
 print(message)
@@ -35,5 +39,4 @@ driver.find_element(By.XPATH,"(//input[@type='text'])[3]").clear()
 
 
 
-
-time.sleep(10)
+time.sleep(5)

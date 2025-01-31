50bcf3c1a19bf1632c2bb9f402b1512ecc274dec
kumaralok001 <105136972+kumaralok001@users.noreply.github.com>
2025-01-31 23:12:34 +0530

    Added new file in pythonSelenium folder
    handles window

---
+++ SeleniumPythonTesting/pythonSelenium/ActionDemo.py
@@ -0,0 +1,23 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
+driver.maximize_window()
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+
+action = ActionChains(driver)
+# action.double_click(driver.find_element(By.))
+# action.context_click()
+# action.drag_and_drop()
+action.move_to_element(driver.find_element(By.ID, "mousehover")).perform()
+# action.context_click(driver.find_element(By.LINK_TEXT, "Top")).perform()
+
+action.move_to_element(driver.find_element(By.LINK_TEXT, "Reload")).click().perform()
+
+
+
+
